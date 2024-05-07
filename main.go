3ff48327f82e88f2c3f764739f1ec23094fb03ea
KaimuSeino <skim07a19yki@icloud.com>
2024-05-07 14:49:30 +0900

    struct and receiver

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import structreceiver "go-basics/struct-receiver"
+
 const secret = "abc"
 
 type Os int
@@ -10,9 +12,40 @@ const (
 	Linux
 )
 
+type Task struct {
+	Title    string
+	Estimate int
+}
+
 func main() {
 	// slicemap.Slice()
 	// slicemap.Map()
+
+	structreceiver.StructReceiver()
+
+	// task1 := Task{
+	// 	Title:    "hello go",
+	// 	Estimate: 3,
+	// }
+
+	// task1.Title = "see you go"
+	// fmt.Printf("Task: Type: %[1]T %+[1]v %v\n", task1, task1.Title)
+
+	// var task2 Task = task1
+	// task2.Title = "new title" // 別のメモリ領域にtask2の値が入っているのがわかる
+	// fmt.Printf("task1_title: %v task2_title: %v\n", task1.Title, task2.Title)
+
+	// task1_p := &Task{
+	// 	Title:    "pointer_task1",
+	// 	Estimate: 2,
+	// }
+	// fmt.Printf("task1_p: type: %T dereference_task1_p: %+v size: %v\n", task1_p, *task1_p, unsafe.Sizeof(task1_p))
+	// task1_p.Title = "changed"
+	// fmt.Printf("task1_p: %+v\n", *task1_p)
+	// var task2_p *Task = task1_p
+	// task2_p.Title = "Changed by task2"
+	// fmt.Printf("task1: %+v\n", *task1_p)
+	// fmt.Printf("task2: %+v\n", *task2_p)
 }
 
 // 一括に値を定義している
---
+++ struct-receiver/struct.go
@@ -0,0 +1,51 @@
+package structreceiver
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Task struct {
+	Title    string
+	Estimate int
+}
+
+func StructReceiver() {
+	task1 := Task{
+		Title:    "hello go",
+		Estimate: 3,
+	}
+
+	task1.Title = "hello gogo"
+	fmt.Printf("Task: Type: %[1]T %+[1]v %v\n", task1, task1.Title)
+
+	var task2 Task = task1
+	task2.Title = "new Title"
+	fmt.Printf("task1: %v task2: %v\n", task1.Title, task2.Title)
+
+	task1_p := &Task{
+		Title:    "pointer_task1",
+		Estimate: 3,
+	}
+	fmt.Printf("task1_p: type: %T dereference: %+v size: %v\n", task1_p, *task1_p, unsafe.Sizeof(task1_p))
+	task1_p.Title = "changed"
+	fmt.Printf("task1_p: %+v\n", *task1_p)
+	var task2_p *Task = task1_p
+	task2_p.Title = "Changed by task2"
+	fmt.Printf("task1: %+v\n", *task1_p)
+	fmt.Printf("task2: %+v\n", *task2_p)
+
+	task1.extendEstimate()
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+	(&task1).extendEstimatePointer()
+	fmt.Printf("task1 value receiver: %+v\n", task1.Estimate)
+
+}
+
+func (task Task) extendEstimate() {
+	task.Estimate += 10
+}
+
+func (taskp *Task) extendEstimatePointer() {
+	taskp.Estimate += 10
+}
